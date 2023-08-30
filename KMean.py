52734fde201bd3ed8e6913fa92a293b06f5fb3bb
lakingz <lakingz@hotmail.com>
2023-08-30 19:11:11 -0400

    KNN + kmean

--- KMean.py
+++ KMean.py
@@ -1,121 +1,97 @@
-class Employee:
+import numpy as np
 
-    raise_amt = 1.04
+#
+# #main  code
+#
+class KMean:
+    def __init__(self, max_iteration, k=3):
+        self.max_iteration = max_iteration
+        self.k = k
+        self.x = None
 
-    def __init__(self, first, last, pay):
-        self.first = first
-        self.last = last
-        self.pay = pay
+    def euclidean_distance(self, x1, x2):
+        return np.sqrt(np.sum((x1 - x2)**2))
 
-    @property
-    def email(self):
-        return '{}.{}@email.com'.format(self.first, self.last)
+    def initialization(self):
+        num_samples, num_features = self.x.shape
+        R = np.zeros((num_samples, self.k))
+        for i in range(num_samples):
+            which_cluster = np.random.randint(0, self.k)
+            R[i, which_cluster] = 1
+        return R
 
-    @property
-    def fullname(self):
-        return '{} {}'.format(self.first, self.last)
+    def get_centroid(self, R):
+        num_samples, num_features = self.x.shape
+        centroids = np.zeros((self.k, num_features))
+        for k in range(self.k):
+            n = np.sum(R, axis=0)
+            centroids[k, :] = np.sum(self.x * R[:, k].reshape(-1, 1), axis=0, keepdims=True) / n[k]
+        return centroids
 
-    @fullname.setter
-    def fullname(self, name):
-        first, last = name.split(' ')
-        self.first = first
-        self.last = last
+    def get_cluster(self, centroids):
+        num_samples, num_features = self.x.shape
+        R = np.zeros((num_samples, self.k))
+        for i in range(num_samples):
+            which_cluster = 0
+            min_distance = float('inf')
+            for k in range(self.k):
+                distance = k_mean.euclidean_distance(self.x[i, :], centroids[k, :])
+                if distance < min_distance:
+                    min_distance = distance
+                    which_cluster = k
+            R[i, which_cluster] = 1
+        return R
 
-    @fullname.deleter
-    def fullname(self):
-        print('Delete name:' + self.fullname)
-        self.first = None
-        self.last = None
+    def onehot_to_list(self, one_hot):
+        __, num_cluster = one_hot.shape
+        cluster_indx = range(num_cluster)
+        cluster_list = np.matmul(one_hot, cluster_indx)
+        return cluster_list
 
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
+    def fit(self, x):
+        self.x = x
+        R = self.initialization()
+        for i in range(self.max_iteration):
+            centroids = self.get_centroid(R)
+            R_new = self.get_cluster(centroids)
+            change = (R - R_new)
+            if change.any() is not True:
+                R = R_new
+        cluster_list = self.onehot_to_list(R)
+        return cluster_list, centroids
 
-    def __repr__(self):
-        return "Employee（‘{}'， '{}', {})".format(self.first, self.last, self.pay)
+#
+# generate sample points
+#
+from sklearn.datasets import make_blobs
+from pandas import DataFrame
+import matplotlib.pyplot as plt
 
-    def __str__(self):
-        return '{} - {}'.format(self.fullname(), self.raise_amt)
+X_train, _ = make_blobs(n_samples=500, centers=3, n_features=2, random_state=20)
+df = DataFrame(dict(x=X_train[:,0], y=X_train[:,1]))
+fig, ax = plt.subplots(figsize=(8,8))
+df.plot(ax=ax, kind='scatter', x='x', y='y')
+plt.xlabel('X_1')
+plt.ylabel('X_2')
+plt.show()
 
-    def printraise(self):
-        print(Employee.raise_amt)
-    
-class Developer(Employee):
-    raise_amt = 1.10
+#
+# fit
+#
+k_mean = KMean(max_iteration=5, k=3)
+print(k_mean.__dict__)
+cluster_list, centroids = k_mean.fit(X_train)
+print(cluster_list)
+print(centroids)
 
-    def __init__(self, first, last, pay, program_lang):
-        super().__init__(first, last, pay)
-        self.program_lang = program_lang
-
-class Manager(Employee):
-
-    def __init__(self, first, last, pay, employees = None):
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees is None
-        else:
-            self.employees = employees
-
-    def add_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
-
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)
-
-    def print_emps(self):
-        for emp in self.employees:
-            print('-->', emp.fullname())
-    
-
-    
-mgr_1 = Manager('sue', 'smith', 90000, [dev_1])
-print(mgr_1.email)
-mgr_1.add_emp(emp_2)
-mgr_1.remove_emp(emp_2)
-print(mgr_1.print_emps())
-
-print(repr(emp_1))
-print(str(emp_1))
-
-print(help(Employee))
-
-emp_1 = Employee('cor', '12', 50000)
-emp_1.first = 'jax'
-print(emp_1.email)
-print(emp_1.fullname)
-emp_1.fullname = 'jaw 4'
-del emp_1.fullname
-
-emp_2 = Employee('test', 'user', 60000)
-print(emp_1 + emp_2)
-
-dev_1 = Developer('tdev', 'user', 60000, 'python')
-
-print(dev_1.pay)
-dev_3.apply_raise()
-print(dev_3.pay)
-print(Employee.__dict__)
-print(emp_1.__str__)
-
-a = 'test'
-def rec(a):
-    if len(a) == 4:
-        return('True')
-    else:
-        return('else')
-leng(a)
-print(emp_1.fullname)
-Employee.rec(emp_1)
-
-def rec():
-    for i in range(10):
-        if i > 8:
-            return i
-rec()
-for i in range(10):
-    print(i)
-
-emp_1.printraise()
-emp_2.raise_amt = 100
-Employee.printraise(emp_2)
+#
+# plot result
+#
+cdict = {0: 'blue', 1: 'green', 2: 'red'}
+fig, ax = plt.subplots()
+for g in np.unique(cluster_list):
+    ix = np.where(cluster_list == g)
+    ax.scatter(X_train[ix, 0], X_train[ix, 1], c=cdict[g], label=g)
+plt.scatter(centroids[:, 0], centroids[:, 1], c='black', marker='^')
+ax.legend()
+plt.show()
